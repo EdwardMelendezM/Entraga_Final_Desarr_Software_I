0bf9ba7ad8d27bea931483a3c2655b918ffdd1af
Edward Melendez Mendigure <192666@unsaac.edu.pe>
2022-08-29 13:42:43 -0500

    Corrigiendo errores de identacion[C

--- ArchivosPHP/Main.php
+++ ArchivosPHP/Main.php
@@ -5,39 +5,39 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="..\Estilos\estilos_main.css" rel="stylesheet" >
-</head>
+    </head>
 
-<body class="principal">
+    <body class="principal">
 
-<section class="section_cabeza">
-<div class="cabeza">
-        <img src="..\Imagenes\unsaac.jpg" class="imagen" width="180" height="180 ">
-        <h1 class="titulo_universidad">UNIVERSIDAD NACIONAL DE SAN ANTONIO DE ABAD DEL CUSCO</h1>
-</div>
-</section>
+        <section class="section_cabeza">
+            <div class="cabeza">
+                    <img src="..\Imagenes\unsaac.jpg" class="imagen" width="180" height="180 ">
+                    <h1 class="titulo_universidad">UNIVERSIDAD NACIONAL DE SAN ANTONIO DE ABAD DEL CUSCO</h1>
+            </div>
+        </section>
 
-<div class="clearfix"> </div>
-<div class="cabezal_div">
-    <div class="cuerpo">
-        <div >
-            <h3 class="Titulo_Cuerpo"> INGRESAR LOS CSV  <h3>
-            <form action="Operaciones.php" method ="POST" enctype="multipart/form-data">
-                <h3 >LISTA DE ALUMNOS MATRICULADOS</h3>
-                <input type="file"  name="Matriculados2022" class="form_dentro">
+        <div class="clearfix"> </div>
+        <div class="cabezal_div">
+            <div class="cuerpo">
+                <div >
+                    <h3 class="Titulo_Cuerpo"> INGRESAR LOS CSV  <h3>
+                    <form action="Operaciones.php" method ="POST" enctype="multipart/form-data">
+                        <h3 >LISTA DE ALUMNOS MATRICULADOS</h3>
+                        <input type="file"  name="Matriculados2022" class="form_dentro">
 
-                <br>
-                <h3 >LISTA DE PROFESORES DEL 2022</h3>
-                <input type="file"  name="Docentes2022" class="form_dentro">
+                        <br>
+                        <h3 >LISTA DE PROFESORES DEL 2022</h3>
+                        <input type="file"  name="Docentes2022" class="form_dentro">
 
-                <br>
-                <h3>LISTA DE TUTURADOS 2021</h3>
-                <input type="file"  name="Tutorados2021" class="form_dentro">
-                <br><br>
-                <input type="submit" name ="submit"    value="Subir" class="form_buttom">
-            </form>
-            
-        </div> 
-    </div>
-</div>  
-</body>
+                        <br>
+                        <h3>LISTA DE TUTURADOS 2021</h3>
+                        <input type="file"  name="Tutorados2021" class="form_dentro">
+                        <br><br>
+                        <input type="submit" name ="submit"    value="Subir" class="form_buttom">
+                    </form>
+                    
+                </div> 
+            </div>
+        </div>  
+    </body>
 </html>
--- ArchivosPHP/Main_2.php
+++ ArchivosPHP/Main_2.php
@@ -5,41 +5,41 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="..\Estilos\estilos_main2.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<section class="section_cabeza">
-<div class="cabeza">
-        <img src="..\Imagenes\unsaac.jpg" class="imagen" width="180" height="180 ">
-        <h1 class="titulo_universidad">UNIVERSIDAD NACIONAL DE SAN ANTONIO DE ABAD DEL CUSCO</h1>
-</div>
-</section>
+    </head>
+    <body>
+        <section class="section_cabeza">
+            <div class="cabeza">
+                    <img src="..\Imagenes\unsaac.jpg" class="imagen" width="180" height="180 ">
+                    <h1 class="titulo_universidad">UNIVERSIDAD NACIONAL DE SAN ANTONIO DE ABAD DEL CUSCO</h1>
+            </div>
+        </section>
 
-<div class="clearfix"> </div>
+        <div class="clearfix"> </div>
 
-<div class="principal">
+        <div class="principal">
 
-    <div class="row">
-        <div class = "funciones">
-            <h1>FUNCIONALIDADES</h1> 
-            <h2>Visualizar Csv</h2>
-            <a href="Alumnos.php" class="buttom" >ALUMNOS MATRICULADOS 2022-I</a>
-            <div class="clearfix2"> </div>
-            <a href="Profesores.php" class="buttom">DOCENTES 2022-I</a>
-            <div class="clearfix2">  </div>
-            <a href="Tutorados.php" class="buttom">TUTORADOS 2022-I</a>
+            <div class="row">
+                <div class = "funciones">
+                    <h1>FUNCIONALIDADES</h1> 
+                    <h2>Visualizar Csv</h2>
+                    <a href="Alumnos.php" class="buttom" >ALUMNOS MATRICULADOS 2022-I</a>
+                    <div class="clearfix2"> </div>
+                    <a href="Profesores.php" class="buttom">DOCENTES 2022-I</a>
+                    <div class="clearfix2">  </div>
+                    <a href="Tutorados.php" class="buttom">TUTORADOS 2022-I</a>
 
-            <br>
+                    <br>
 
-            <h2>Operaciones</h2>
-            <a href="No_Considerados.php" class="buttom">ALUMNOS NO CONSIDERADOS PARA TUTORIA SEMESTRE 2022 - I</a>
-            <div class="clearfix2"> </div>
-            <a href="NuevosTutorados.php" class="buttom">DISTRIBUCION BALANCEADA DE TUTORIAS</a>
-            <div class="clearfix2"> </div>
-            <a href="Main.php" class="form_buttom"><h2>SALIR</h2></a> 
-            <div class="clearfix2"> </div>
+                    <h2>Operaciones</h2>
+                    <a href="No_Considerados.php" class="buttom">ALUMNOS NO CONSIDERADOS PARA TUTORIA SEMESTRE 2022 - I</a>
+                    <div class="clearfix2"> </div>
+                    <a href="NuevosTutorados.php" class="buttom">DISTRIBUCION BALANCEADA DE TUTORIAS</a>
+                    <div class="clearfix2"> </div>
+                    <a href="Main.php" class="form_buttom"><h2>SALIR</h2></a> 
+                    <div class="clearfix2"> </div>
 
-        </div> 
-    </div>
-</div>  
-</body>
+                </div> 
+            </div>
+        </div>  
+    </body>
 </html>
